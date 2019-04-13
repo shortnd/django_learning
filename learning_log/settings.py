298d5ae3d79ec23498dab41e64444c3541dc7af5
shortnd <ococncol@gmail.com>
2019-04-13 18:56:58 -0400

    trying again

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -142,17 +142,18 @@ BOOTSTRAP3 = {
 }
 
 # Heroku settings
-import dj_database_url
-DATABASES = {
-    'default': dj_database_url.config(default='postgres://localhost')
-}
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
+    # Allow all host headers
+    ALLOWED_HOSTS = ['*']
 
-import django_heroku
-django_heroku.settings(locals())
+    import django_heroku
+    django_heroku.settings(locals())
 
